316a78a9bf170cd215f1ee4788a1153c1bb87272
giuliaserrano <44641111+giuliaserrano@users.noreply.github.com>
2018-11-01 11:49:46 -0300

    Add files via upload

---
+++ Exercicio Revisao/questao1.py
@@ -0,0 +1,10 @@
+Nota1= float(input('Digite a primeira nota:'))
+Nota2= float(input('Digite a segunda nota:'))
+Nota3= float(input('Digite a terceira nota:'))
+Media= (Nota1+Nota2+Nota3)/3
+if Media==10:
+  print('Aprovado com destinção')
+if Media>=7:
+  print('Aprovado')
+else:
+  print('Reprovado')
