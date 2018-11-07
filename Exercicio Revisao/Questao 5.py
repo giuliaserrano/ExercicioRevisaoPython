1b1747532793494b261a737c10b0094cf2c3eb6b
giuliaserrano <44641111+giuliaserrano@users.noreply.github.com>
2018-11-07 21:26:29 -0200

    Add files via upload

---
+++ Exercicio Revisao/Questao 5.py
@@ -0,0 +1,7 @@
+lista = []
+
+for i in range(5):
+    a = int(input())
+    lista.append(a)
+
+print(lista)
---
+++ Exercicio Revisao/Questao 6.py
@@ -0,0 +1,10 @@
+nota1= float(input("digite a nota"))
+nota2= float(input("digite a nota"))
+nota3= float(input("digite a nota"))
+nota4= float(input("digite a nota"))
+media= (nota1+nota2+nota3+nota4)/4
+print("Primeira nota: %.1f"% (nota1))
+print("Segunda nota: %.1f"% (nota2))
+print("Terceira nota: %.1f"% (nota3))
+print("Quarta nota: %.1f"% (nota4))
+print("Média:%.1f"%(media))
